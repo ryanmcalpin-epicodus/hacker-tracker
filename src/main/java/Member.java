c5c0f2d991347d0ed6f141675149a832294f4ef7
Ryan McAlpin <rhmcalpin@gmail.com>
2017-03-24 13:43:19 -0700

    returns all members with all(), clears all instances of member with clear(), returns member by id with getId()

--- src/main/java/Member.java
+++ src/main/java/Member.java
@@ -1,11 +1,30 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Member {
   private String mName;
+  private static List<Member> instances = new ArrayList<Member>();
+  private int mId;
 
   public Member(String name) {
     mName = name;
+    instances.add(this);
+    mId = instances.size();
   }
 
   public String getName() {
     return mName;
+  }
+
+  public int getId() {
+    return mId;
+  }
+
+  public static List<Member> all() {
+    return instances;
+  }
+
+  public static void clear() {
+    instances.clear();
   }
 }
--- src/test/java/MemberTest.java
+++ src/test/java/MemberTest.java
@@ -3,6 +3,11 @@ import static org.junit.Assert.*;
 
 public class MemberTest {
 
+  @Before
+  public void tearDown() {
+    Member.clear();
+  }
+
   @Test
   public void member_instantiatesCorrectly_true() {
     Member member = new Member("Ryan");
@@ -13,5 +18,26 @@ public class MemberTest {
   public void getName_returnsName_String() {
     Member member = new Member("Ryan");
     assertEquals("Ryan", member.getName());
+  }
+
+  @Test
+  public void all_returnsAllInstances_true() {
+    Member member = new Member("Ryan");
+    Member member2 = new Member("Hannah");
+    assertEquals(true, Member.all().contains(member));
+    assertEquals(true, Member.all().contains(member2));
+  }
+
+  @Test
+  public void clear_removesAllInstances_0() {
+    Member member = new Member("Ryan");
+    Member.clear();
+    assertEquals(0, Member.all().size());
+  }
+
+  @Test
+  public void getId_memberInstantiatesWithId_1() {
+    Member member = new Member("Ryan");
+    assertEquals(1, member.getId());
   }
 }
