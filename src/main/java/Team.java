c20df63bd59e6cd9ab0ae47ef9719ea2ae133ad8
Ryan McAlpin <rhmcalpin@gmail.com>
2017-03-24 09:55:51 -0700

    return all instances of Team, remove all instances of Team

--- src/main/java/Team.java
+++ src/main/java/Team.java
@@ -1,11 +1,24 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Team {
   private String mName;
+  private static List<Team> instances = new ArrayList<Team>();
 
   public Team(String name) {
     mName = name;
+    instances.add(this);
   }
 
   public String getName() {
     return mName;
+  }
+
+  public static List<Team> all() {
+    return instances;
+  }
+
+  public static void clear() {
+    instances.clear();
   }
 }
--- src/test/java/TeamTest.java
+++ src/test/java/TeamTest.java
@@ -14,4 +14,19 @@ public class TeamTest {
     Team team = new Team("The Hacks");
     assertEquals("The Hacks", team.getName());
   }
+
+  @Test
+  public void all_returnsAllInstances_true() {
+    Team team = new Team("The Hacks");
+    Team otherTeam = new Team("The Others");
+    assertEquals(true, Team.all().contains(team));
+    assertEquals(true, Team.all().contains(otherTeam));
+  }
+
+  @Test
+  public void clear_removesAllInstances_0() {
+    Team team = new Team("The Hacks");
+    Team.clear();
+    assertEquals(0, Team.all().size());
+  }
 }
